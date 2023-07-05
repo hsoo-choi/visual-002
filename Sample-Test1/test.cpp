d026344f633a507ff0d6ea611bbcfabb5812ede7
hsoo.choi <choihszg@gmail.com>
2023-07-05 15:14:37 +0900

    [feature] 37p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,3 +9,10 @@ TEST(PrimeFacotrs, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFacotrs, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
