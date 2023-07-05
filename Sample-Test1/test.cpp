b05cf0aee2282917837d12a20eba12262c943a5d
hsoo.choi <choihszg@gmail.com>
2023-07-05 15:16:19 +0900

    [refactoring] 38p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,15 +4,20 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFacotrs, Of1) {
+class PrimeFixture:public testing::Test
+{
+public:
 	PrimeFactor prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFacotrs, Of2) {
-	PrimeFactor prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
